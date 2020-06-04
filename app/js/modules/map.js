f15e95148f53d32f7f37b7d0e498dd886b5e7bde
Kauri <you@example.com>
2020-06-04 17:05:56 +0300

    Map-style updated

--- app/js/modules/map.js
+++ app/js/modules/map.js
@@ -3,7 +3,7 @@
 const MapFunctions = {
 
     // Function to initialize the map
-    // Map-styles updated 4.6.2020
+    //  Map-styles updated 4.6.2020
 
     init: () => {
         const newMap = L.map('mapid').setView([60.1699, 24.9384], 11);
